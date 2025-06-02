31a3ddb40f0936ace6de8d8a17a9eaba12b3df17
ShakibRamez <shakibramiz1@icloud.com>
2025-06-02 13:15:46 +0430

    task compelted

--- aretmatic.js
+++ aretmatic.js
@@ -12,3 +12,15 @@ console.log(score);
 
 let sum = 5 + 3 * 2 - 1 * (1 + 1);
 console.log(sum);
+
+
+// task
+
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2);
+console.log(num1 % num2);
+
+let total = 10;
+total++;
+console.log(total);
