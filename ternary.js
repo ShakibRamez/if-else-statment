77771a4e192235f23506fc12ba118674d2fd23bf
ShakibRamez <shakibramiz1@icloud.com>
2025-06-02 13:32:14 +0430

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,15 @@
+// let age = 17;
+
+// if(age >= 18){
+//     console.log("You can vote");
+// }else{
+//     console.log("you cant voete");
+// }
+
+// let message = age  >=  18 ? "you can vote" : "you cant vote";
+// console.log(message);
+
+
+// let password = "asdfg12345";
+// let message =(password === "asdfg12345") ? 'success' : 'failed';
+// console.log(message);
