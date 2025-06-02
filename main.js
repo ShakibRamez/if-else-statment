3a96ad5e4e4eb8d28cedbc36d36930906068bf57
ShakibRamez <shakibramiz1@icloud.com>
2025-06-02 12:42:41 +0430

    if else statment

---
+++ main.js
@@ -0,0 +1,37 @@
+// if else statment
+
+// if(true){
+//     console.log("this is true");
+// }
+
+
+// if(1 < 10){
+//     console.log("1 is less than 10");
+// }
+
+
+// if(10 > 1){
+//     console.log("10 is greater than 1");
+// }
+
+
+// if else
+
+// let game = "mario";
+
+// if(game === "sonic"){
+//     console.log("this condition is true");
+// }else{
+//     console.log("this condition is false");
+// }
+
+// let roll = 2;
+//  if(roll === 4){
+//     console.log("you rolled a four");
+//  }else if(roll === 5){
+//     console.log("you rolled a five");
+//  }else if(roll === 6){
+//     console.log("you rolled a six");
+//  }else{
+//     console.log("you rolled a number that is not 4,5, or 6");
+//  }
