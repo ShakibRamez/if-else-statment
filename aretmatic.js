1929eced146758ef9fada6cee5a613f0971ce420
ShakibRamez <shakibramiz1@icloud.com>
2025-06-02 13:11:14 +0430

    aretmathic

---
+++ aretmatic.js
@@ -0,0 +1,14 @@
+console.log(5+2);
+console.log(5-2);
+console.log(5/2);
+console.log(5*2);
+
+console.log(5%2);
+console.log(5**3);
+
+let score = 99;
+score++;
+console.log(score);
+
+let sum = 5 + 3 * 2 - 1 * (1 + 1);
+console.log(sum);
