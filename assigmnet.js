4bcf2bf82a4668cf036c2a460bd83439a2ae8fc2
ShakibRamez <shakibramiz1@icloud.com>
2025-06-02 13:01:24 +0430

    assigmnet operator

---
+++ assigmnet.js
@@ -0,0 +1,31 @@
+// Simple Assignment
+// let x = 10;
+
+
+// Add and Assign
+// let x = 5;
+// x += 3;
+
+
+// Subtract and Assign
+// let x = 10;
+// x -= 4; 
+
+
+// Multiply and Assign
+// let x = 6;
+// x *= 2;
+
+// Divide and Assign
+// let x = 20;
+// x /= 5;
+
+// Modulus and Assign
+
+// let x = 10;
+// x %= 3; 
+
+
+// Exponentiation and Assign
+// let x = 3;
+// x **= 2;
